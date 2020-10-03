6154321f77dcc34ba929043edf41227aac43d6d5
Sindhu Yella <yellasindhu09@gmail.com>
2020-10-03 21:34:48 +0530

    Feat(ui):Add navigation drawer and empty FileViewModel

--- app/src/main/java/com/example/smartnote/MainActivity.kt
+++ app/src/main/java/com/example/smartnote/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.example.smartnote
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.smartnote.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -10,10 +14,45 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        setupNavigation()
+    }
+
+    private fun setupNavigation() {
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+        navController = findNavController(R.id.nav_host_fragment)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+        NavigationUI.setupWithNavController(binding.sideNavigationDrawer, navController)
+        binding.sideNavigationDrawer.setNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.books_item->{
+                    navController.navigate(R.id.booksFragment)
+                }
+                R.id.qrscanner_item->{
+                    navController.navigate(R.id.qrscannerFragment)
+                }
+            }
+            true
+        }
+    }
+
+    override fun onBackPressed() {
+        if(binding.drawerLayout.isDrawerOpen(GravityCompat.START)){
+            binding.drawerLayout.closeDrawer(GravityCompat.START)
+            return
+        }
+        super.onBackPressed()
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(navController, binding.drawerLayout)
     }
 }
---
+++ app/src/main/java/com/example/smartnote/fragments/QRScannerFragment.kt
@@ -0,0 +1,18 @@
+package com.example.smartnote.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.smartnote.R
+
+class QRScannerFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_q_r_scanner, container, false)
+    }
+}
