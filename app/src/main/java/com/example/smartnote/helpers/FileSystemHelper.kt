273a4c66f493f56f74007768225624a6209186a0
Dhruv Kachhadia <59411325+dhruv1294@users.noreply.github.com>
2020-10-10 14:43:01 +0530

    feat: add file system helper
    * Initiated fileSystemHelper Class
    
    * feat(init):Implemented make folder function
    
    * feat(init): Added hilt support for context
    
    * feat(init): remove unused imports

---
+++ app/src/main/java/com/example/smartnote/helpers/FileSystemHelper.kt
@@ -0,0 +1,18 @@
+package com.example.smartnote.helpers
+
+import android.content.Context
+import android.util.Log
+import dagger.hilt.android.qualifiers.ApplicationContext
+import java.io.File
+
+class FileSystemHelper(@ApplicationContext var context: Context) {
+
+     fun makeFolder(folderName: String,filePath: String){
+        val medFolder = File(context.filesDir.toString() + filePath, folderName)
+         Log.i("myTag",context.filesDir.toString()+ filePath)
+         if (!medFolder.exists()) {
+             medFolder.mkdirs()
+         }
+
+    }
+}
