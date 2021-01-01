daa1551b619dd39e51ebb21c673b15f08c958ed7
dhruv <dhruvkachhadia@gmail.com>
2021-01-01 18:51:36 +0530

    chore

--- app/src/main/java/com/example/smartnote/adapters/PagesAdapter.kt
+++ app/src/main/java/com/example/smartnote/adapters/PagesAdapter.kt
@@ -3,7 +3,6 @@ package com.example.smartnote.adapters
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
 import com.example.smartnote.databinding.PageItemBinding
 import com.squareup.picasso.Picasso
 import java.io.File
