4c5f37a8f43e5a855ef0b6db725b88d4318dce16
Pradeep S <pradeep.deep7@gmail.com>
2021-02-15 13:28:46 +0530

    feat: change backup folder structure to include date

--- app/src/main/java/com/example/smartnote/repository/BackupRepository.kt
+++ app/src/main/java/com/example/smartnote/repository/BackupRepository.kt
@@ -5,59 +5,65 @@ import com.example.smartnote.helpers.Constants.TYPE_FOLDER
 import com.example.smartnote.helpers.DriveServiceHelper
 import com.google.android.gms.common.api.ApiException
 import kotlinx.coroutines.tasks.await
+import java.text.DateFormat
+import java.util.*
 
 class BackupRepository {
 
-    companion object {
-        const val TAG = "BackupRepository"
-    }
+  companion object {
+    const val TAG = "BackupRepository"
+  }
 
-    suspend fun uploadPDF(
-        helper: DriveServiceHelper,
-        pdfPath: String
-    ) {
-        try {
-            val details = pdfPath.split("/")
-            val pdfName = details[details.size - 2] + ".pdf"
-            val subjectName = details[details.size - 3]
-            val bookName = details[details.size - 4]
-            val baseFolderName = "Smart Note"
-            val fileList = helper.queryFiles().await()
-            val files = fileList!!.files
-            var bookFolderID: String? = null
-            var subjectFolderID: String? = null
-            var baseFolderID: String? = null
-            for (file in files) {
-                if (file["mimeType"] == TYPE_FOLDER) {
-                    when (file["name"]) {
-                        bookName -> {
-                            bookFolderID = file["id"] as String
-                        }
-                        baseFolderName -> {
-                            baseFolderID = file["id"] as String
-                        }
-                    }
-                }
+  suspend fun uploadPDF(
+    helper: DriveServiceHelper,
+    pdfPath: String
+  ) {
+    try {
+      val details = pdfPath.split("/")
+      val pdfName = details[details.size - 2] + ".pdf"
+      val subjectName = details[details.size - 3]
+      val bookName = details[details.size - 4]
+      val dateName = DateFormat.getDateInstance(DateFormat.MEDIUM).format(Calendar.getInstance().time)
+      val baseFolderName = "Smart Note"
+      val fileList = helper.queryFiles().await()
+      val files = fileList!!.files
+      var bookFolderID: String?
+      var subjectFolderID: String?
+      var dateFolderID: String?
+      var baseFolderID: String? = null
+      for (file in files) {
+        if (file["mimeType"] == TYPE_FOLDER) {
+          when (file["name"]) {
+            baseFolderName -> {
+              baseFolderID = file["id"] as String
             }
-            if (baseFolderID == null) {
-                baseFolderID = helper.createFolder(baseFolderName, "root").await()
-            }
-            if (bookFolderID == null) {
-                bookFolderID = helper.createFolder(bookName, baseFolderID).await()
-            }
-            subjectFolderID = helper.getFileChildren(bookFolderID, subjectName).await()
-            if (subjectFolderID == null) {
-                subjectFolderID = helper.createFolder(subjectName, bookFolderID).await()
-            }
-            val existingFileID = helper.getFileChildren(subjectFolderID, pdfName).await()
-            if (existingFileID != null) {
-                helper.deleteFile(existingFileID)
-            }
-            val fileID = helper.createPDF(pdfName, subjectFolderID, pdfPath).await()
-            Log.i(TAG, "fileList: $fileList")
-            Log.i(TAG, "fileID: $fileID")
-        } catch (e: ApiException) {
-            Log.w(TAG, "listFilesResult:failed code=" + e.statusCode)
+          }
         }
+      }
+      if (baseFolderID == null) {
+        baseFolderID = helper.createFolder(baseFolderName, "root").await()
+      }
+      dateFolderID = helper.getFileChildren(baseFolderID, dateName).await()
+      if (dateFolderID == null) {
+        dateFolderID = helper.createFolder(dateName, baseFolderID).await()
+      }
+      bookFolderID = helper.getFileChildren(dateFolderID, bookName).await()
+      if (bookFolderID == null) {
+        bookFolderID = helper.createFolder(bookName, dateFolderID).await()
+      }
+      subjectFolderID = helper.getFileChildren(bookFolderID, subjectName).await()
+      if (subjectFolderID == null) {
+        subjectFolderID = helper.createFolder(subjectName, bookFolderID).await()
+      }
+      val existingFileID = helper.getFileChildren(subjectFolderID, pdfName).await()
+      if (existingFileID != null) {
+        helper.deleteFile(existingFileID)
+      }
+      val fileID = helper.createPDF(pdfName, subjectFolderID, pdfPath).await()
+      Log.i(TAG, "fileList: $fileList")
+      Log.i(TAG, "fileID: $fileID")
+    } catch (e: ApiException) {
+      Log.w(TAG, "listFilesResult:failed code=" + e.statusCode)
     }
+  }
 }
